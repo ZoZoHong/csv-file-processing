f4c2e7816e8eb712f714abad90a04dbdb4a2dc10
ZoZoHong <1161113466@qq.com>
2021-11-03 15:30:16 +0800

    version frist over

--- py_csv_procesing.py
+++ py_csv_procesing.py
@@ -10,6 +10,7 @@ from matplotlib.ticker import FuncFormatter
 from adjustText import adjust_text
 from scipy import stats
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+from matplotlib import ticker 
 import math
 import timeit
 start = timeit.default_timer()
@@ -132,6 +133,7 @@ df = pd.read_csv('./output/data_startAtSiteNum.csv')
 
 Unit = df.iloc[0]
 df = df.apply(pd.to_numeric, errors='coerce').fillna(df.mean())
+df = df.loc[:, ~df.columns.str.contains('Unnamed')]
 df1 = df[:3]
 
 LimitL = df1.iloc[1]
@@ -202,19 +204,11 @@ def plotOfMe(data, Name):
     #     plt.plot(x, y)
     # 正态分布曲线有bug 需调整
     # 生成 正态分布曲线
-
-    # y轴 设置为 % 显示
-    formatter = FuncFormatter(to_percent)
-    ax = plt.gca()
-    ax.yaxis.set_major_formatter(formatter)
-    major_ax = pow(10,(math.floor(math.log10(abs(max(result))))))
-    xminorLocator   = MultipleLocator(round(major_ax/5,2))
-    ax.xaxis.set_minor_locator(xminorLocator)
     # 划分上下限区间
     # 权重修改
     weights = np.ones_like(result)/float(len(result))
     # 保存数据到ans中,完成图形自适应设置
-    ans = plt.hist(result, 100, weights=weights,
+    ans = plt.hist(result, 20, weights=weights,
                    rwidth=0.7, color='orange')
     # 坐标轴刻度  自适应, x轴即可
     # ax = plt.gca()
@@ -230,15 +224,25 @@ def plotOfMe(data, Name):
         # 显示多数
         elif(ans[0][i] >= 0.05):
             datatostr = str(round(ans[0][i]*100, 2))+"%"
-            texts.append( plt.text(ans[1][i], ans[0][i], datatostr))
-        # 文字重叠 adjust_text
+            texts.append( plt.text(ans[1][i], ans[0][i], datatostr))           
+    # y轴 设置为 % 显示 , 设置副刻度值和等比例输出图形
+    formatter = FuncFormatter(to_percent)
+    ax = plt.gca()
+    ax.yaxis.set_major_formatter(formatter)
+    
+    if(result_max-result_min != 0):
+        major_ax = ax.get_xticks()
+        # major_ax = pow(10,(math.floor(math.log10(abs(result_max-result_min)))))
+        xminorLocator   = MultipleLocator(round((major_ax[1]-major_ax[0])/4,4))
+        ax.xaxis.set_minor_locator(xminorLocator)
+    # 文字重叠 adjust_text
     adjust_text(texts,lim=50,arrowprops=dict( arrowstyle='->', lw= 1, color='red'))
     # 网格线 , 坐标轴标签, 标题
     plt.grid()
-    plt.xlabel('Measure')
-    plt.ylabel('Percent')
+    # plt.xlabel('Measure')
+    # plt.ylabel('Percent')
     plt.title(Name+ ' , Unit : %s \n μ : %4.3f , sigma : %4.3f , 6sigma : (%4.3f,%4.3f) \n max : %4.3f , min : %4.3f \n LimitL : %.3f , LimitU : %.3f , Cp : %.3f , Cpk : %.3f' 
-              % (Unit[Name],mu, sigma, mu-6*sigma, mu+6*sigma, result_max, result_min,L,U,Cp,Cpk))
+              % (Unit[Name],mu, sigma, mu-6*sigma, mu+6*sigma, result_max, result_min,L,U,Cp,Cpk),loc='left')
 
 
 def generate_visible_data(data,start):
@@ -267,33 +271,21 @@ def Statistical_parameters(data,Name):
     Cp = cp(U,L,sigma)
     return result,mu,sigma,result_max,result_min,L,U,Cpk,Cp
 def plotOfsite(site1,site2, Name):
-    # result1,mu1,sigma1,result_max1,result_min1,L1,U1,Cpk1,Cp1 = Statistical_parameters(site1,Name)
-    # result2,mu2,sigma2,result_max2,result_min2,L2,U2,Cpk2,Cp2 = Statistical_parameters(site2,Name)
     result1 = site1[Name]
     result2 = site2[Name]
-    # 先取出行，然后在取列
-    # 会出现记录了39就失效了，40也加入计算，所以会多了很多比例 
-    # len1 = len(df_major_site1[df_major_site1.eval('I_WARN') < LimitL['I_WARN']]['I_WARN'])
-    # len2 = len(df_major_site1[df_major_site1.eval('I_WARN') > LimitU['I_WARN']]['I_WARN'])
-    # len3 = len(df_major_site1['I_WARN'])
-    # len4 = len(df_major_site1)
-    # print(df_major_site1[df_major_site1.eval('I_WARN') < LimitL['I_WARN']]['I_WARN'])
-    # print(df_major_site1[df_major_site1.eval('I_WARN') > LimitU['I_WARN']]['I_WARN'])
-    # len1 = len(df_major_site2[df_major_site2.eval('I_WARN') < LimitL['I_WARN']]['I_WARN'])
-    # len2 = len(df_major_site2[df_major_site2.eval('I_WARN') > LimitU['I_WARN']]['I_WARN'])
-    # len3 = len(df_major_site2['I_WARN'])
-    # len4 = len(df_major_site2)
-
-    fail_rate1 = (len(site1[site1.eval(Name) < LimitL[Name]][Name]) + len(site1[site1.eval(Name) > LimitU[Name]][Name]) ) /len(site1[Name])
-    fail_rate2 = (len(site2[site2.eval(Name) < LimitL[Name]][Name]) + len(site2[site2.eval(Name) > LimitU[Name]][Name]) ) /len(site2[Name])
+    # print(getattr(site1, Name))
+    
+    fail_rate1 = (len(site1[getattr(site1, Name) < LimitL[Name]][Name]) + len(site1[getattr(site1, Name) > LimitU[Name]][Name]) ) /len(site1[Name])
+    fail_rate2 = (len(site2[getattr(site2, Name) < LimitL[Name]][Name]) + len(site2[getattr(site2, Name) > LimitU[Name]][Name]) ) /len(site2[Name])
     
     fail_rate1,fail_rate2 = str(round(100*fail_rate1, 2))+"%" , str(round(100*fail_rate2, 2))+"%"
     # y轴 设置为 % 显示
     formatter = FuncFormatter(to_percent)
     ax = plt.gca()
     ax.yaxis.set_major_formatter(formatter)
-    major_ax = pow(10,(math.floor(math.log10(abs(max(result1))))))
-    xminorLocator   = MultipleLocator(round(major_ax/5,2))
+    xminorLocator = ticker.AutoMinorLocator(4) 
+    # major_ax = pow(10,(math.floor(math.log10(abs(max(result1))))))
+    # xminorLocator   = MultipleLocator(round(major_ax/5,2))
     ax.xaxis.set_minor_locator(xminorLocator)
 
     # 划分上下限区间
@@ -310,41 +302,10 @@ def plotOfsite(site1,site2, Name):
     
     # 网格线 , 坐标轴标签, 标题
     plt.grid()
-    plt.xlabel('Measure')
-    plt.ylabel('Percent')
+    # plt.ylabel('Percent')
     plt.legend(loc='upper right')
-    plt.title(Name + 'fail_rate: SITE1 -> %s SITE2 ->%s ' %(fail_rate1,fail_rate2) )
+    plt.title(Name + '\nFail_rate: SITE1 -> %s SITE2 ->%s ' %(fail_rate1,fail_rate2) )
 
-
-# def plotOfsite(site1,site2, Name):
-#     # result1,mu1,sigma1,result_max1,result_min1,L1,U1,Cpk1,Cp1 = Statistical_parameters(site1,Name)
-#     # result2,mu2,sigma2,result_max2,result_min2,L2,U2,Cpk2,Cp2 = Statistical_parameters(site2,Name)
-#     result1 = site1[Name]
-#     result2 = site2[Name]
-    
-
-
-#     # y轴 设置为 % 显示
-#     formatter = FuncFormatter(to_percent)
-#     plt.gca().yaxis.set_major_formatter(formatter)
-
-#     # 划分上下限区间
-#     # 权重修改
-#     weights1 = np.ones_like(result1)/float(len(result1))
-#     weights2 = np.ones_like(result2)/float(len(result2))
-#     # 保存数据到ans中,完成图形自适应设置
-#     # plt.hist(result1, 100, weights=weights1,histtype='barstacked',
-#     #                rwidth=1, alpha= 0.6,label='SITE1')
-#     # plt.hist(result2, 100, weights=weights2,histtype='barstacked',
-#     #                rwidth=1, alpha= 0.6,label= 'SITE2' )
-#     plt.hist([result1,result2],20,weights=[weights1,weights2],label=['SITE1','SITE2'])
-    
-#     # 网格线 , 坐标轴标签, 标题
-#     plt.grid()
-#     plt.xlabel('Measure')
-#     plt.ylabel('Percent')
-#     plt.legend(loc='upper right')
-#     plt.title(Name)
 
 
 generate_visible_data(df_bin1,'Pin10BST')
